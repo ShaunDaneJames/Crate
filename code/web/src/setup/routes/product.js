c5e2b18e32cf2e6955ca5da9305e7e792ea25a57
Shawn Truesdale <shawnctruesdale@gmail.com>
2021-02-03 17:27:07 -0700

    Install Depens, format

--- code/web/src/setup/routes/product.js
+++ code/web/src/setup/routes/product.js
@@ -1,10 +1,10 @@
 // App Imports
-import Detail from '../../modules/product/Detail'
+import Detail from '../../modules/product/Detail';
 
 // Product routes
 export default {
   product: {
-    path: (slug = ':slug') => (`/product/${ slug }`),
-    component: Detail
-  }
-}
+    path: (slug = ':slug') => `/product/${slug}`,
+    component: Detail,
+  },
+};
