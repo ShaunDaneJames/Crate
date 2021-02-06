cc4ffb163655a7dde488ccd6258710d46f59a1cd
Alyssa Bull <alyssabull@gmail.com>
2021-02-06 14:34:41 -0700

    Add survey questions data file

---
+++ code/web/src/modules/survey/surveyQuestions.js
@@ -0,0 +1,153 @@
+export const surveyQuestions = [
+  {
+    id: 1,
+    question: 'Which Spice Girl matches your style?',
+    answers: [
+      {
+        text: 'Scary Spice',
+        image: './assets/scarySpice.jpg',
+        category: 'grunge/punk'
+      },
+      {
+        text: 'Sporty Spice',
+        image: './assets/sportySpice.jpg',
+        category: 'streetwear/sporty'
+      },
+      {
+        text: 'Posh Spice',
+        image: './assets/poshSpice.jpg',
+        category: 'classic'
+      },
+      {
+        text: 'Baby Spice',
+        image: './assets/babySpice.jpg',
+        category: 'casual'
+      },
+      {
+        text: 'Ginger Spice',
+        image: './assets/gingerSpice.jpg',
+        category: 'bohemian'
+      }
+    ]
+  },{
+    id: 2,
+    question: 'What activity do you do most?',
+    answers: [
+      {
+        text: 'Jamming out at a rock concert',
+        image: './assets/rockConcert.jpg',
+        category: 'grunge/punk'
+      },
+      {
+        text: 'Running in the great outdoors',
+        image: './assets/running.jpg',
+        category: 'streetwear/sporty'
+      },
+      {
+        text: 'Fancy cocktails with friends',
+        image: './assets/fancyCocktails.jpg',
+        category: 'classic'
+      },
+      {
+        text: 'Coffee shop chilling',
+        image: './assets/coffeeShop.jpg',
+        category: 'casual'
+      },
+      {
+        text: 'Meditating',
+        image: './assets/meditation.jpg',
+        category: 'bohemian'
+      },
+    ]
+  },{
+    id: 3,
+    question: 'What does your ideal home look like?',
+    answers: [
+      {
+        text: 'Dumpster Chic',
+        image: './assets/dumpsterChic.jpg',
+        category: 'grunge/punk'
+      },
+      {
+        text: 'Nice Home Gym',
+        image: './assets/homeGym.jpg',
+        category: 'streetwear/sporty'
+      },
+      {
+        text: 'Elegant Finishes',
+        image: './assets/elegantFinishes.jpg',
+        category: 'classic'
+      },
+      {
+        text: 'Suburban Oasis',
+        image: './assets/suburbanOasis.jpg',
+        category: 'casual'
+      },
+      {
+        text: 'Magical Tree House',
+        image: './assets/treeHouse.jpg',
+        category: 'bohemian'
+      }
+    ]
+  },{
+    id: 4,
+    question: 'What city best suites you?',
+    answers: [
+      {
+        text: 'Berlin, Germany',
+        image: './assets/berlin.jpg',
+        category: 'grunge/punk'
+      },
+      {
+        text: 'Tokyo, Japan',
+        image: './assets/tokyo.jpg',
+        category: 'streetwear/sporty'
+      },
+      {
+        text: 'Paris, France',
+        image: './assets/paris.jpg',
+        category: 'classic'
+      },
+      {
+        text: 'Denver, Colorado',
+        image: './assets/denver.jpg',
+        category: 'casual'
+      },
+      {
+        text: 'Amsterdam, Netherlands',
+        image: './assets/amsterdam.jpg',
+        category: 'bohemian'
+      }
+    ]
+  },{
+    id: 5,
+    question: 'What movie do you like?',
+    answers: [
+      {
+        text: 'Lords of Dogtown',
+        image: './assets/lordsOfDogtown.jpg',
+        category: 'grunge/punk'
+      },
+      {
+        text: 'Step Up',
+        image: './assets/stepUp.jpg',
+        category: 'streetwear/sporty'
+      },
+      {
+        text: 'Breakfast at Tiffany\'s',
+        image: './assets/breakfastAtTiffanys.jpg',
+        category: 'classic'
+      },
+      {
+        text: 'Groundhog Day',
+        image: './assets/groundhogDay.jpg',
+        category: 'casual'
+      },
+      {
+        text: 'Across the Universe',
+        image: './assets/acrossTheUniverse.jpg',
+        category: 'bohemian'
+      }
+    ]
+  }
+]
