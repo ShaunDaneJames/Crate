d198c3a46775951876863d414eb7f775ef77d71b
Shawn Truesdale <shawnctruesdale@gmail.com>
2021-02-08 11:32:55 -0700

    Add pathing to route to user survey on sub click

--- code/web/src/setup/routes/user.js
+++ code/web/src/setup/routes/user.js
@@ -1,38 +1,45 @@
 // App Imports
 // The imports below refer to the components that each of the routes below will link to
-import Login from '../../modules/user/Login'
-import Signup from '../../modules/user/Signup'
-import Profile from '../../modules/user/Profile'
-import Subscriptions from '../../modules/user/Subscriptions'
+import Login from '../../modules/user/Login';
+import Signup from '../../modules/user/Signup';
+import Profile from '../../modules/user/Profile';
+import Subscriptions from '../../modules/user/Subscriptions';
+import Survey from '../../modules/survey/Survey';
 
 // User routes
 // The routes below refer to all the possible routes related to user
 // When the page has a route match with one of the following paths it renders the corresponding component
-// This file's purpose is to separate out all the possible user routes 
+// This file's purpose is to separate out all the possible user routes
 // The App component (when built) will map over all possible routes (in all the files) and put them in a switch
 export default {
   login: {
     path: '/user/login',
-    component: Login
+    component: Login,
   },
 
   signup: {
     path: '/user/signup',
-    component: Signup
+    component: Signup,
   },
 
   profile: {
     path: '/user/profile',
     component: Profile,
-    auth: true
+    auth: true,
   },
 
   subscriptions: {
     path: '/user/subscriptions',
     component: Subscriptions,
-    auth: true
-  }
-}
+    auth: true,
+  },
+
+  survey: {
+    path: '/user/survey',
+    component: Survey,
+    auth: true,
+  },
+};
 
 // TODO:
 // Add a new route called stylePreferences
