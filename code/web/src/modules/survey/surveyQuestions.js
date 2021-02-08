033e6ee334e6a984aef77ec292e21631d069a850
Shawn Truesdale <shawnctruesdale@gmail.com>
2021-02-08 16:04:12 -0700

    Change category on questions, Create recordAnswer()

--- code/web/src/modules/survey/surveyQuestions.js
+++ code/web/src/modules/survey/surveyQuestions.js
@@ -6,148 +6,152 @@ export const surveyQuestions = [
       {
         text: 'Scary Spice',
         image: './assets/scarySpice.jpg',
-        category: 'grunge/punk'
+        category: 'grunge',
       },
       {
         text: 'Sporty Spice',
         image: './assets/sportySpice.jpg',
-        category: 'streetwear/sporty'
+        category: 'sporty',
       },
       {
         text: 'Posh Spice',
         image: './assets/poshSpice.jpg',
-        category: 'classic'
+        category: 'classic',
       },
       {
         text: 'Baby Spice',
         image: './assets/babySpice.jpg',
-        category: 'casual'
+        category: 'casual',
       },
       {
         text: 'Ginger Spice',
         image: './assets/gingerSpice.jpg',
-        category: 'bohemian'
-      }
-    ]
-  },{
+        category: 'bohemian',
+      },
+    ],
+  },
+  {
     id: 2,
     question: 'What activity do you do most?',
     answers: [
       {
         text: 'Jamming out at a rock concert',
         image: './assets/rockConcert.jpg',
-        category: 'grunge/punk'
+        category: 'grunge',
       },
       {
         text: 'Running in the great outdoors',
         image: './assets/running.jpg',
-        category: 'streetwear/sporty'
+        category: 'sporty',
       },
       {
         text: 'Fancy cocktails with friends',
         image: './assets/fancyCocktails.jpg',
-        category: 'classic'
+        category: 'classic',
       },
       {
         text: 'Coffee shop chilling',
         image: './assets/coffeeShop.jpg',
-        category: 'casual'
+        category: 'casual',
       },
       {
         text: 'Meditating',
         image: './assets/meditation.jpg',
-        category: 'bohemian'
+        category: 'bohemian',
       },
-    ]
-  },{
+    ],
+  },
+  {
     id: 3,
     question: 'What does your ideal home look like?',
     answers: [
       {
         text: 'Dumpster Chic',
         image: './assets/dumpsterChic.jpg',
-        category: 'grunge/punk'
+        category: 'grunge',
       },
       {
         text: 'Nice Home Gym',
         image: './assets/homeGym.jpg',
-        category: 'streetwear/sporty'
+        category: 'sporty',
       },
       {
         text: 'Elegant Finishes',
         image: './assets/elegantFinishes.jpg',
-        category: 'classic'
+        category: 'classic',
       },
       {
         text: 'Suburban Oasis',
         image: './assets/suburbanOasis.jpg',
-        category: 'casual'
+        category: 'casual',
       },
       {
         text: 'Magical Tree House',
         image: './assets/treeHouse.jpg',
-        category: 'bohemian'
-      }
-    ]
-  },{
+        category: 'bohemian',
+      },
+    ],
+  },
+  {
     id: 4,
     question: 'What city best suites you?',
     answers: [
       {
         text: 'Berlin, Germany',
         image: './assets/berlin.jpg',
-        category: 'grunge/punk'
+        category: 'grunge',
       },
       {
         text: 'Tokyo, Japan',
         image: './assets/tokyo.jpg',
-        category: 'streetwear/sporty'
+        category: 'sporty',
       },
       {
         text: 'Paris, France',
         image: './assets/paris.jpg',
-        category: 'classic'
+        category: 'classic',
       },
       {
         text: 'Denver, Colorado',
         image: './assets/denver.jpg',
-        category: 'casual'
+        category: 'casual',
       },
       {
         text: 'Amsterdam, Netherlands',
         image: './assets/amsterdam.jpg',
-        category: 'bohemian'
-      }
-    ]
-  },{
+        category: 'bohemian',
+      },
+    ],
+  },
+  {
     id: 5,
     question: 'What movie do you like?',
     answers: [
       {
         text: 'Lords of Dogtown',
         image: './assets/lordsOfDogtown.jpg',
-        category: 'grunge/punk'
+        category: 'grunge',
       },
       {
         text: 'Step Up',
         image: './assets/stepUp.jpg',
-        category: 'streetwear/sporty'
+        category: 'sporty',
       },
       {
-        text: 'Breakfast at Tiffany\'s',
+        text: "Breakfast at Tiffany's",
         image: './assets/breakfastAtTiffanys.jpg',
-        category: 'classic'
+        category: 'classic',
       },
       {
         text: 'Groundhog Day',
         image: './assets/groundhogDay.jpg',
-        category: 'casual'
+        category: 'casual',
       },
       {
         text: 'Across the Universe',
         image: './assets/acrossTheUniverse.jpg',
-        category: 'bohemian'
-      }
-    ]
-  }
-]
+        category: 'bohemian',
+      },
+    ],
+  },
+];
